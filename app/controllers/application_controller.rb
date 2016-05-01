f481ec94aa5916f5c26e9b56f282a4715f0e7ae8
Zachary Friedman <zafriedman@gmail.com>
2016-04-30 17:03:43 -0700

    Remove commented out private method in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  private
-
-  # def after_sign_in_path_for(resource_or_scope)
-  #   step_path(current_user.aasm.current_state)
-  # end
 end
